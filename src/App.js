eb31bb4f4c95db35c87d38c30a9836bc0e9a44e6
Blackscure <Wekesabuyahi@gmail.com>
2018-12-16 12:37:08 +0300

    app .js file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Projects from './Components/Projects';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        My App
+        My App 
+        <Projects/>
       </div>
     );
   }
